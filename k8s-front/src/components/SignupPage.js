ed310a9d1487195a119d2019febf5aa283e60a9a
uijin00 <tlsdmlwls21@naver.com>
2025-11-18 17:13:43 +0900

    SingupPage Edit

--- k8s-front/src/components/SignupPage.js
+++ k8s-front/src/components/SignupPage.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './SignupPage.css';
 
-const USER_API_BASE_URL = '/api';
+const USER_API_BASE_URL = process.env.REACT_APP_USER_API_URL || 'http://localhost:8080';
 
 export default function SignupPage() {
   const navigate = useNavigate();
